c58ea207ba0f6b4092ff15f86d189e7358b8fada
kot <kostiaGm@gmail.com>
2019-01-19 07:58:30 +0200

    FC

---
+++ SiteFrontendBundle/SiteFrontendBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\AKS\SiteFrrontendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Created by PhpStorm.
+ * User: kot
+ * Date: 19.01.19
+ * Time: 7:19
+ */
+class SiteFrontendBundle extends Bundle
+{
+
+}
